f8a355c05ea9c83ae0bf2dd948f655ef854c1b5a
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-11-07 22:40:47 -0500

    Update Carrito_ComprasServiceImpl.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/Carrito_ComprasServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/Carrito_ComprasServiceImpl.java
@@ -11,5 +11,30 @@ import java.util.Optional;
 
 @Service
 public class Carrito_ComprasServiceImpl implements ICarrito_ComprasService {
+    @Autowired
+    private ICarrito_ComprasRepository cpR;
+    @Override
+    public void insert(Carrito_Compras carrito_compras) {
+        cpR.save(carrito_compras);
+    }
 
+    @Override
+    public List<Carrito_Compras> list() {
+        return cpR.findAll();
+    }
+
+    @Override
+    public void delete(int idCarrito_Compras) {
+        cpR.deleteById(idCarrito_Compras);
+    }
+
+    @Override
+    public Optional<Carrito_Compras> listarId(int idCarrito_Compras) {
+        return Optional.of(cpR.findById(idCarrito_Compras).orElse(new Carrito_Compras()));
+    }
+
+    @Override
+    public List<Carrito_Compras> buscarPrenda(String nombrePrenda) {
+        return cpR.searchPrenda(nombrePrenda);
+    }
 }
