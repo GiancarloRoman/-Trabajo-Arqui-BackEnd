992f621195f9fa156131f00f432af62b9d6c0379
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-11-07 21:31:09 -0500

    Update-prenda-servicesimpls

--- src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/PrendaServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/PrendaServiceImpl.java
@@ -13,5 +13,45 @@ import java.util.Optional;
 @Service
 public class PrendaServiceImpl implements IPrendaService {
 
+    @Autowired
+    private IPrendaRepository pR;
+    @Override
+    public void insert(Prenda prenda) {
+        pR.save(prenda);
+    }
 
+    @Override
+    public List<Prenda> list() {
+        return pR.findAll();
+    }
+
+    @Override
+    public void delete(int idPrenda) {
+        pR.deleteById(idPrenda);
+    }
+
+    @Override
+    public Optional<Prenda> listarId(int idPrenda) {
+        return Optional.of(pR.findById(idPrenda).orElse(new Prenda()));
+    }
+
+    @Override
+    public List<Prenda> search(String nombrePrenda) {
+        return pR.searchPrenda(nombrePrenda);
+    }
+
+    @Override
+    public List<Prenda> buscarTienda(String nombreTienda) {
+        return pR.searchTienda(nombreTienda);
+    }
+
+    @Override
+    public List<Prenda> buscarMarca(String nombreMarca) {
+        return pR.searchMarca(nombreMarca);
+    }
+
+    @Override
+    public List<Prenda> buscarTalla(String letraTalla) {
+        return pR.searchTalla(letraTalla);
+    }
 }
