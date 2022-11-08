9a8e5526d75f7a238843d1660e13a63785deaa4d
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-11-07 22:37:42 -0500

    Update Marca.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/entities/Marca.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/entities/Marca.java
@@ -5,5 +5,33 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Marca")
 public class Marca {
+    public Marca() {
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMarca;
 
+    @Column(name = "nombreMarca", length = 50, nullable = false)
+    private String nombreMarca;
+
+    public Marca(int idMarca, String nombreMarca) {
+        this.idMarca = idMarca;
+        this.nombreMarca = nombreMarca;
+    }
+
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    public String getNombreMarca() {
+        return nombreMarca;
+    }
+
+    public void setNombreMarca(String nombreMarca) {
+        this.nombreMarca = nombreMarca;
+    }
 }
