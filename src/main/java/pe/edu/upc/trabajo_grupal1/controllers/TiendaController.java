9249d12468fc4a7eb372b8ee20758d741a38c79b
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-11-07 19:19:23 -0500

    Update TiendaController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TiendaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TiendaController.java
@@ -10,5 +10,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/tiendas")
 public class TiendaController {
+    @Autowired
+    private ITiendaService tService;
 
+    @PostMapping
+    public void registrar(@RequestBody Tienda t) {
+        tService.insert(t);
+    }
+    @GetMapping
+    public List<Tienda> listar() {
+        return tService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        tService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Tienda tienda){
+        tService.insert(tienda);
+    }
+    @PostMapping("/buscar")
+    public List<Tienda> buscar(@RequestBody Tienda tienda){
+        List<Tienda> lista;
+        tienda.setNombreTienda(tienda.getNombreTienda());
+        lista=tService.search(tienda.getNombreTienda());
+        return lista;
+    }
 }
