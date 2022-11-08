b609bfd23c3f1f82490701f8f821c78312e400f7
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-11-07 23:32:57 -0500

    Update TallaController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TallaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TallaController.java
@@ -2,9 +2,9 @@ package pe.edu.upc.trabajo_grupal1.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.trabajo_grupal1.entities.Prenda;
-import pe.edu.upc.trabajo_grupal1.entities.Talla;
-import pe.edu.upc.trabajo_grupal1.serviceinterfaces.ITallaService;
+import pe.edu.upc.trabajo_grupal.entities.Prenda;
+import pe.edu.upc.trabajo_grupal.entities.Talla;
+import pe.edu.upc.trabajo_grupal.serviceinterfaces.ITallaService;
 
 import java.util.List;
 
