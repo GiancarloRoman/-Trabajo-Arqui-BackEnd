1427101719a27f545a97eb7c31ecc2eeed1d071a
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-11-07 23:44:03 -0500

    Update Talla.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/entities/Talla.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/entities/Talla.java
@@ -5,5 +5,34 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Talla")
 public class Talla {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTalla;
 
+    @Column(name = "letraTalla", length = 50, nullable = false)
+    private String letraTalla;
+
+    public Talla() {
+    }
+
+    public Talla(int idTalla, String letraTalla) {
+        this.idTalla = idTalla;
+        this.letraTalla = letraTalla;
+    }
+
+    public int getIdTalla() {
+        return idTalla;
+    }
+
+    public void setIdTalla(int idTalla) {
+        this.idTalla = idTalla;
+    }
+
+    public String getLetraTalla() {
+        return letraTalla;
+    }
+
+    public void setLetraTalla(String letraTalla) {
+        this.letraTalla = letraTalla;
+    }
 }
