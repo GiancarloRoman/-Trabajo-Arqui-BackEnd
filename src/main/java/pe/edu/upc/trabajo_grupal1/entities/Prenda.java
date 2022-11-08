f6f608609b2dba21337c161eabdd88756a22519c
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-11-07 21:26:34 -0500

    Update-prenda-entities

--- src/main/java/pe/edu/upc/trabajo_grupal1/entities/Prenda.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/entities/Prenda.java
@@ -1,9 +1,57 @@
-package pe.edu.upc.trabajo_grupal1.entities;
+package pe.edu.upc.trabajo_grupal1.controllers;
 
-import javax.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.trabajo_grupal1.entities.Prenda;
+import pe.edu.upc.trabajo_grupal1.serviceinterfaces.IPrendaService;
 
-@Entity
-@Table(name = "Prenda")
-public class Prenda {
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
+@RestController
+@RequestMapping("/prendas")
+public class PrendaController {
+    @Autowired
+    private IPrendaService pService;
+
+    @PostMapping
+    public void registrar(@RequestBody Prenda p) {
+        pService.insert(p);
+    }
+    @GetMapping
+    public List<Prenda> listar() {
+        return pService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        pService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Prenda prenda){
+        pService.insert(prenda);
+    }
+  @PostMapping("/buscar")
+    public List<Prenda> buscar(@RequestBody Prenda prenda)throws ParseException {
+        List<Prenda> listaPrendas;
+        listaPrendas = pService.search(prenda.getNombrePrenda());
+
+        if (listaPrendas.isEmpty()) {
+
+            listaPrendas = pService.buscarTienda(prenda.getTienda().getNombreTienda());
+            if (listaPrendas.isEmpty()) {
+
+                listaPrendas = pService.buscarMarca(prenda.getMarca().getNombreMarca());
+            }
+            if (listaPrendas.isEmpty()) {
+
+                listaPrendas = pService.buscarTalla(prenda.getTalla().getLetraTalla());
+            }
+        }
+        return listaPrendas;
+    }
+    @GetMapping("/{id}")
+    public Optional<Prenda> listarId(@PathVariable("id") Integer id) {
+        return pService.listarId(id);
+    }
 }
