f47dec236202c95813478e2383cb2013ab8e3325
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-11-07 22:39:50 -0500

    Update ICarrito_ComprasService.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/ICarrito_ComprasService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/ICarrito_ComprasService.java
@@ -7,5 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ICarrito_ComprasService {
-
+    public void insert(Carrito_Compras carrito_compras);
+    List<Carrito_Compras> list();
+    public void delete(int idCarrito_Compras);
+    public Optional<Carrito_Compras> listarId(int idCarrito_Compras);
+    List<Carrito_Compras> buscarPrenda(String nombrePrenda);
 }
