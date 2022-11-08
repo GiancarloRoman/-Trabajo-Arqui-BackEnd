e19cb78124631c0f9a480268418636e66ee33d34
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-11-07 20:08:55 -0500

    Update IClienteService.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IClienteService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IClienteService.java
@@ -6,5 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IClienteService {
-
+    public void insert(Cliente cliente);
+    List<Cliente>list();
+    public void delete(int idPropietario);
+    public Optional<Cliente> listarId(int idCliente);
+    List<Cliente>search(String nombreCliente);
 }
+
