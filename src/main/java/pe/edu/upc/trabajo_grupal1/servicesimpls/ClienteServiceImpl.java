f51430cf4a1fa5f2fa759b19276c554fb115939e
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-11-07 20:09:25 -0500

    Update ClienteServiceImpl.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/ClienteServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/ClienteServiceImpl.java
@@ -11,5 +11,30 @@ import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
+    @Autowired
+    private IClienteRepository cR;
+    @Override
+    public void insert(Cliente cliente) {
+        cR.save(cliente);
+    }
 
+    @Override
+    public List<Cliente> list() {
+        return cR.findAll();
+    }
+
+    @Override
+    public void delete(int idCliente) {
+        cR.deleteById(idCliente);
+    }
+
+    @Override
+    public Optional<Cliente> listarId(int idCliente) {
+        return Optional.of(cR.findById(idCliente).orElse(new Cliente()));
+    }
+
+    @Override
+    public List<Cliente> search(String nombreCliente) {
+        return cR.search(nombreCliente);
+    }
 }
