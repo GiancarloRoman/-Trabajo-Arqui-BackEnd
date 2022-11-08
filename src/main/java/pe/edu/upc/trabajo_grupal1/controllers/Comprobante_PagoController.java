ca802a25a758da5cf96d65b98bb438831cfd3009
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-11-07 19:22:10 -0500

    Update Comprobante_PagoController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/Comprobante_PagoController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/Comprobante_PagoController.java
@@ -12,5 +12,37 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/comprobantes_pago")
 public class Comprobante_PagoController {
+    @Autowired
+    private IComprobante_ComprasService cService;
 
+    @PostMapping
+    public void registrar(@RequestBody Comprobante_Pago cp) {
+        cService.insert(cp);
+    }
+    @GetMapping
+    public List<Comprobante_Pago> listar() {
+        return cService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        cService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Comprobante_Pago comprobante_pago){
+        cService.insert(comprobante_pago);
+    }
+    @PostMapping("/buscar")
+    public List<Comprobante_Pago> buscar(@RequestBody Comprobante_Pago comprobante_pago)throws ParseException {
+        List<Comprobante_Pago> listaComprobantes;
+        listaComprobantes = cService.buscarTipoComprobante(comprobante_pago.getTipoComprobante());
+        if (listaComprobantes.isEmpty()) {
+
+            listaComprobantes = cService.buscarRazonSocial(comprobante_pago.getRazonsocial());
+        }
+        return listaComprobantes;
+    }
+    @GetMapping("/{id}")
+    public Optional<Comprobante_Pago> listarId(@PathVariable("id") Integer id) {
+        return cService.listarId(id);
+    }
 }
