02975cfde450fafb31af70c3459aadcdd3f3a4eb
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-11-07 23:49:41 -0500

    Update Comprobante_ComprasServiceImpl.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/Comprobante_ComprasServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/Comprobante_ComprasServiceImpl.java
@@ -11,5 +11,36 @@ import java.util.Optional;
 
 @Service
 public class Comprobante_ComprasServiceImpl implements IComprobante_ComprasService {
+    @Autowired
+    private IComprobante_PagoRepository cpR;
 
+    @Override
+    public void insert(Comprobante_Pago comprobante_pago) {
+        cpR.save(comprobante_pago);
+    }
+
+    @Override
+    public List<Comprobante_Pago> list() {
+        return cpR.findAll();
+    }
+
+    @Override
+    public void delete(int idComprobante_Pago) {
+        cpR.deleteById(idComprobante_Pago);
+    }
+
+    @Override
+    public Optional<Comprobante_Pago> listarId(int idComprobante_Pago) {
+        return Optional.of(cpR.findById(idComprobante_Pago).orElse(new Comprobante_Pago()));
+    }
+
+    @Override
+    public List<Comprobante_Pago> buscarTipoComprobante(String nombrecomprobante) {
+        return cpR.searchComprobante_Pago_Tipo(nombrecomprobante);
+    }
+
+    @Override
+    public List<Comprobante_Pago> buscarRazonSocial(String nombreRazonSocial) {
+        return cpR.searchComprobante_Pago_Razon(nombreRazonSocial);
+    }
 }
