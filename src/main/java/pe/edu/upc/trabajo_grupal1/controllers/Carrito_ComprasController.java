24d25388846f37b4ff536866329a7d4ab067613b
Alonso2103 <106572266+Alonso2103@users.noreply.github.com>
2022-11-07 20:39:52 -0500

    Update Carrito_ComprasController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/Carrito_ComprasController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/Carrito_ComprasController.java
@@ -13,5 +13,33 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/carrito_de_compras")
 public class Carrito_ComprasController {
+    @Autowired
+    private ICarrito_ComprasService cpService;
 
+    @PostMapping
+    public void registrar(@RequestBody Carrito_Compras cp) {
+        cpService.insert(cp);
+    }
+    @GetMapping
+    public List<Carrito_Compras> listar() {
+        return cpService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        cpService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Carrito_Compras carrito_compras){
+        cpService.insert(carrito_compras);
+    }
+    @PostMapping("/buscar")
+    public List<Carrito_Compras> buscar(@RequestBody Carrito_Compras carrito_compras)throws ParseException {
+        List<Carrito_Compras> listaCarrito_Compras;
+        listaCarrito_Compras = cpService.buscarPrenda(carrito_compras.getPrenda().getNombrePrenda());
+        return listaCarrito_Compras;
+    }
+    @GetMapping("/{id}")
+    public Optional<Carrito_Compras> listarId(@PathVariable("id") Integer id) {
+        return cpService.listarId(id);
+    }
 }
