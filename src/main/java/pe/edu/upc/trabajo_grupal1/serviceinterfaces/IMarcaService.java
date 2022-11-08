3bf3ffeda9e265c890c0dd02bcacb26f97742c9f
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-11-07 22:39:25 -0500

    Update IMarcaService.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IMarcaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IMarcaService.java
@@ -6,5 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IMarcaService {
-
+    public void insert(Marca marca);
+    List<Marca> list();
+    public void delete(int idMarca);
+    public Optional<Marca> listarId(int idMarca);
+    List<Marca>search(String nombreMarca);
 }
