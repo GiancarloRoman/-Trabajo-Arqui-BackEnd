c5d50b205428c8b85f945975e35707e870478284
Giancarlo <93389927+GiancarloRoman@users.noreply.github.com>
2022-11-07 19:21:08 -0500

    Update TiendaServiceImpl.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/TiendaServiceImpl.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/servicesimpls/TiendaServiceImpl.java
@@ -11,5 +11,30 @@ import java.util.Optional;
 
 @Service
 public class TiendaServiceImpl implements ITiendaService {
+    @Autowired
+    private ITiendaRepository tR;
+    @Override
+    public void insert(Tienda tienda) {
+        tR.save(tienda);
+    }
 
+    @Override
+    public List<Tienda> list() {
+        return tR.findAll();
+    }
+
+    @Override
+    public void delete(int idTienda) {
+        tR.deleteById(idTienda);
+    }
+
+    @Override
+    public Optional<Tienda> listarId(int idTienda) {
+        return Optional.of(tR.findById(idTienda).orElse(new Tienda()));
+    }
+
+    @Override
+    public List<Tienda> search(String nombreTienda) {
+        return tR.search(nombreTienda);
+    }
 }
