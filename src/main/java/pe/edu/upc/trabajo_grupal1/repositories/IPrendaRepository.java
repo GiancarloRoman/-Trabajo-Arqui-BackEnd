0b8808a9039345deba5b229cfa0039b8a8c6b9c3
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-11-07 21:29:21 -0500

    Update-prenda-repositories

--- src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IPrendaRepository.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/repositories/IPrendaRepository.java
@@ -10,5 +10,15 @@ import java.util.List;
 
 @Repository
 public interface IPrendaRepository extends JpaRepository<Prenda,Integer> {
+    @Query("from Prenda p where p.nombrePrenda like %:nombrePrenda")
+    List<Prenda> searchPrenda(@Param("nombrePrenda") String nombrePrenda);
 
+    @Query("from Prenda p where p.tienda.nombreTienda like %:nombreTienda")
+    List<Prenda> searchTienda(@Param("nombreTienda") String nombreTienda);
+
+    @Query("from Prenda p where p.marca.nombreMarca like %:nombreMarca")
+    List<Prenda> searchMarca(@Param("nombreMarca") String nombreMarca);
+
+    @Query("from Prenda p where p.talla.letraTalla like %:letraTalla")
+    List<Prenda> searchTalla(@Param("letraTalla") String letraTalla);
 }
