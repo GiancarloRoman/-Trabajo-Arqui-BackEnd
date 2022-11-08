c48541ad7c36705cebf42afe0020794183bb1cc9
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-11-07 23:32:28 -0500

    Update TallaController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TallaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/TallaController.java
@@ -11,5 +11,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/tallas")
 public class TallaController {
+    @Autowired
+    private ITallaService tService;
 
+    @PostMapping
+    public void registrar(@RequestBody Talla t) {
+        tService.insert(t);
+    }
+    @GetMapping
+    public List<Talla> listar() {
+        return tService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        tService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Talla talla){
+        tService.insert(talla);
+    }
+    @PostMapping("/buscar")
+    public List<Talla> buscar(@RequestBody Talla talla){
+        List<Talla> lista;
+        talla.setLetraTalla(talla.getLetraTalla());
+        lista=tService.search(talla.getLetraTalla());
+        return lista;
+    }
 }
