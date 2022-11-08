1319e8aa3ac6e5624220bff5fbed71f5a5ef3d42
RenzoPortillo <113268088+RenzoPortillo@users.noreply.github.com>
2022-11-07 21:30:28 -0500

    Update-prenda-serviceinterfaces

--- src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IPrendaService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IPrendaService.java
@@ -6,5 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IPrendaService {
+    public void insert(Prenda prenda);
+    List<Prenda>list();
+    public void delete(int idPrenda);
+    public Optional<Prenda> listarId(int idPrenda);
+    List<Prenda>search(String nombrePrenda);
 
+    List<Prenda> buscarTienda(String nombreTienda);
+
+    List<Prenda> buscarMarca(String nombreMarca);
+    List<Prenda> buscarTalla(String letraTalla);
 }
