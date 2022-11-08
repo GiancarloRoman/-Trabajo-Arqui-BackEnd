06ef5b0644da81f0af5d092f6267493abf45c903
VVILEL <93390996+VVILEL@users.noreply.github.com>
2022-11-07 22:36:43 -0500

    Update MarcaController.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/controllers/MarcaController.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/controllers/MarcaController.java
@@ -11,6 +11,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/marcas")
 public class MarcaController {
+    @Autowired
+    private IMarcaService mService;
 
+    @PostMapping
+    public void registrar(@RequestBody Marca m) {
+        mService.insert(m);
+    }
+    @GetMapping
+    public List<Marca> listar() {
+        return mService.list();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        mService.delete(id);
+    }
+    @PutMapping
+    public void modificar(@RequestBody Marca marca){
+        mService.insert(marca);
+    }
+    @PostMapping("/buscar")
+    public List<Marca> buscar(@RequestBody Marca marca){
+        List<Marca> lista;
+        marca.setNombreMarca(marca.getNombreMarca());
+        lista=mService.search(marca.getNombreMarca());
+        return lista;
+    }
 }
- 
+
