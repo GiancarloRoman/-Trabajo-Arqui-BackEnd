9c593743769ca0c8b29e16e36d758f54ccd54982
Annsherly <88410194+Annsherly1705@users.noreply.github.com>
2022-11-07 23:49:12 -0500

    Update IComprobante_ComprasService.java

--- src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IComprobante_ComprasService.java
+++ src/main/java/pe/edu/upc/trabajo_grupal1/serviceinterfaces/IComprobante_ComprasService.java
@@ -6,5 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IComprobante_ComprasService {
-
+    public void insert(Comprobante_Pago comprobante_pago);
+    List<Comprobante_Pago> list();
+    public void delete(int idComprobante_Pago);
+    public Optional<Comprobante_Pago> listarId(int idComprobante_Pago);
+    List<Comprobante_Pago> buscarTipoComprobante(String nombrecomprobante);
+    List<Comprobante_Pago> buscarRazonSocial(String nombreRazonSocial);
 }
